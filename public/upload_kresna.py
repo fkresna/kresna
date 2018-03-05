ffa32a0c6b49657edc69b4c5ac6da5ed86185ebc
Freddy <fkresna@gmail.com>
2018-03-05 20:33:40 +0000

    add trackiing code

--- public/upload_kresna.py
+++ public/upload_kresna.py
@@ -4,6 +4,4 @@ s3 = boto3.resource('s3')
 
 pages = ['index.html','computertutor.html']
 for page in pages:
-  object_acl = s3.ObjectAcl('kresna.xyz',page)
-  response = object_acl.put(ACL='public-read')
-
+  s3.Bucket('kresna.xyz').upload_file(page,page)
